e38c98940fa4365b0ea9f12ca317be4bfd088b04
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2022-11-16 00:54:36 -0300

    Componentes de order e orderboard estilizados

---
+++ src/components/OrdersBoard/index.tsx
@@ -0,0 +1,25 @@
+import { Board, OrdersContainer } from './styles';
+
+export function OrdersBoard() {
+  return (
+    <>
+      <Board>
+        <header>
+          <span>⏰</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+        <OrdersContainer>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 items</span>
+          </button>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 items</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+    </>
+  );
+}
